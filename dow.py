f05d87ea6668ec1a6c84b2585dea538724e5479b
raimundob16 <53773141+raimundob16@users.noreply.github.com>
2023-09-27 21:57:48 -0300

    atividade de casa

---
+++ dow.py
@@ -0,0 +1,6 @@
+arquivo= float (input("digite o tamanho do aquivo em MB: "))
+velocidade = float (input("digite a velocidade MB/s: "))
+tamanho = arquivo*8
+velocidade1 = (tamanho / velocidade)
+
+print("Velocidade", velocidade1)
---
+++ tempo_distancia.py
@@ -0,0 +1,5 @@
+distancia = float(input("Digite a distancia"))
+velocida = float(input("Digite a velocidade"))
+temp_minutos = (distancia/velocida)*60
+tempo_hora = temp_minutos/60
+print("Voce ira gastar: ",temp_minutos," Horas")
