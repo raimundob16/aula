b9a5ea00c6e2b9e908c06107d5e02fbd32d55c5e
raimundob16 <53773141+raimundob16@users.noreply.github.com>
2023-09-28 22:05:38 -0300

    corrigi a ta

--- taxa_de_pais.py
+++ taxa_de_pais.py
@@ -1,3 +1,10 @@
-
-pais1 = 80000
-pais2 = 20000
+paisA = 80000
+paisB = 200000
+anos = 0
+cresA =  0.03
+cresB = 0.015 
+while (paisA < paisB):
+    anos += 1
+    paisA = paisA + (paisA * cresA)
+    paisB = paisB + (paisB * cresB)
+print("Após %i anos o país A ultrapassou o país B em número de habitantes." % anos)
